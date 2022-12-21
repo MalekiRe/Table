07095ef1648c36b4c24632baa88e4a08e6407e2f
MalekiRe <pocmalek@gmail.com>
2022-12-21 13:30:58 -0800

    save this spot

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,13 @@
 mod parser;
-
+use chumsky::Parser;
 use std::ops::Range;
 use terminal_emoji::Emoji;
 //
 // mod parser;
 
 fn main() {
-    print_hearts(vec![3..7, 14..18]);
-    print_hearts(vec![2..8, 13..19]);
-    print_hearts(vec![1..9, 12..20]);
-    for i in 0..3 {
-        print_hearts(vec![0..21]);
-    }
-    for i in 1..11 {
-        print_hearts(vec![i..21-i]);
-    }
+    let src = std::fs::read_to_string("src/test.tbl").unwrap();
+    let p = parser::file_parser();
+    println!("{:#?}", p.parse(src).unwrap());
 
-
-}
-fn print_hearts(ranges: Vec<Range<u32>>) {
-    for i in 0..21 {
-        let mut did = false;
-        for range in &ranges {
-            if range.contains(&i) {
-                heart();
-                did = true;
-            }
-        }
-        if !did {
-            space()
-        }
-    }
-    print!("\n");
-}
-fn space() {
-    print!("🦀a");
-}
-fn heart() {
-    print!("❤️");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,71 +1,108 @@
 use chumsky::{Parser, text};
-use chumsky::prelude::{recursive, Simple};
+use chumsky::prelude::{end, just, none_of, one_of, recursive, Simple};
 use chumsky::text::{ident, TextParser};
 
 #[derive(Debug)]
-enum Statement {
-    ExprStatement(Expr),
-    FnDef(FnDef),
-    LetStatement(LetStatement)
-}
-#[derive(Debug)]
-struct LetStatement {
+pub struct LetStatement {
     var_name: String,
     value: Expr,
 }
-/// this is the equiv of whats normally in a `{ }`, that whole thing including the parenthisis is a block.
 #[derive(Debug)]
-enum Block {
-    StatementsAndExpr {
-        statements: Vec<Statement>,
-        expression: Expr,
-    },
-    Statements {
-        statements: Vec<Statement>,
-    },
-    Expr {
-        expression: Expr,
-    },
-    Empty,
-}
-#[derive(Debug)]
-struct FunctionBody(pub Vec<Block>);
-#[derive(Debug)]
-struct File(pub FunctionBody);
-#[derive(Debug)]
-enum Expr {
-    Literal(Literal),
-    VarIdent(String),
-    FnCall(FnCall),
-}
-#[derive(Debug)]
-enum Literal {
+pub enum Literal {
     I32(i32),
 }
 #[derive(Debug)]
-struct FnDef {
+pub struct FnDef {
     name: String,
     args: Vec<String>,
     body: FunctionBody
 }
 #[derive(Debug)]
-struct FnCall {
+pub struct FnCall {
     name: String,
     args: Vec<Expr>,
 }
 
-fn file_parser() -> impl Parser<char, File, Error = Simple<char>> {
-    //let ident = ident().padded();
-    let function_body = recursive(|function_body| {
-        let expr = recursive(|expr| {
-            let int = text::int(10)
-                .map(|s: String| Expr::Literal(Literal::I32(s.parse().unwrap())))
-                .padded();
-            int
-        });
-        let statement = recursive(|statement| {
-
-        });
-    });
-    unimplemented!();
+#[derive(Debug)]
+pub enum Expr{
+    StatementAExpr(Statement, Box<Expr>),
+    BracedExprBlock(BracedExprBlock),
+    Literal(Literal),
+    FnCall(FnCall),
 }
+#[derive(Debug)]
+pub enum ExprBlock {
+    Expr(Box<Expr>),
+    Braced(BracedExprBlock),
+}
+#[derive(Debug)]
+pub enum StatementBlock {
+    Statements(Vec<Statement>),
+    Braced(BracedStatementBlock),
+}
+#[derive(Debug)]
+pub enum Statement {
+    BracedBlock(BracedStatementBlock),
+    ExprStatment(Box<Expr>),
+    FnDef(Box<FnDef>)
+}
+#[derive(Debug)]
+pub struct BracedStatementBlock(Box<StatementBlock>);
+#[derive(Debug)]
+pub struct BracedExprBlock(Box<ExprBlock>);
+pub fn file_parser() -> impl Parser<char, File, Error = Simple<char>> {
+    let file = recursive(|file| {
+        let atom = {
+            let int = text::int(10).map(|s: String| Expr::Literal(Literal::I32(s.parse().unwrap()))).padded();
+            int
+        };
+        let expr = recursive(|expr| {
+            let statement = recursive(|statement| {
+                expr.clone().then_ignore(just(';')).map(|st| {
+                  Statement::ExprStatment(Box::new(st))
+                })
+            });
+            let expr_statement = statement.clone().then(expr.clone()).map(|(s, e)| {
+               Expr::StatementAExpr(s, Box::new(e))
+            });
+            let no_statement_type = {
+                atom
+            };
+            no_statement_type.then(just(";").ignore_then(expr)).map(|(e1, e2)| {
+                Expr::StatementAExpr(Statement::ExprStatment(Box::new(e2)), Box::new(e1))
+            }).or(no_statement_type)
+        });
+        expr.map(|expr| {
+            File(FunctionBody::Expr(expr))
+        })
+    });
+    file.then_ignore(end())
+}
+#[derive(Debug)]
+pub struct File(FunctionBody);
+#[derive(Debug)]
+pub enum FunctionBody {
+    Expr(Expr),
+    Statement(Statement),
+}
+// fn hi() {
+//     let int = text::int(10)
+//         .map(|s: String| Expr::Literal(Literal::I32(s.parse().unwrap())))
+//         .padded();
+//     int
+// }
+
+// let statement = recursive(|statement| {
+// let expression_statement = expr.clone().then_ignore(just(";").padded()).map(|expression| {
+// Statement::ExprStatement(expression)
+// });
+// expression_statement
+// });
+// let block = recursive(|block| {
+// let statements_and_expr = statement.clone().repeated().at_least(1).then(expr.clone())
+// .map(|(statements, expression)| { Block::ExprBlock(ExprBlock::StatementsAndExpr {statements, expression: Box::new(expression)})});
+// let single_expr = expr.clone().map(|expr| {Block::ExprBlock(ExprBlock::Expr(Box::new(expr)))});
+// let statements_no_expr = statement.clone().repeated().at_least(1).map(|statements| Block::Statements(StatementsBlock(statements)));
+// statements_and_expr
+// });
+// block.map(|a| FunctionBody(vec![a]))
